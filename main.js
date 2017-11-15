5d6af64294a3bc82145bc89a5821092c677f8d44
postni <1524300@stud.hs-mannheim.de>
2017-11-15 14:22:14 +0100

    wininstall
    win-install

--- main.js
+++ main.js
@@ -5,6 +5,16 @@ const datastore = require('./backend/data').datastore
 
 // Module to control application life.
 const app = electron.app
+
+//Desktop-Anwendung
+// this should be placed at top of main.js to handle setup events quickly
+if (handleSquirrelEvent(app)) {
+    // squirrel event handled and app will exit in 1000ms, so don't do anything else
+    return;
+}
+
+
+
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
@@ -67,6 +77,76 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+
+/////////////////////////
+//      Win-Install   ///
+/////////////////////////
+
+function handleSquirrelEvent(application) {
+  if (process.argv.length === 1) {
+      return false;
+  }
+
+  const ChildProcess = require('child_process');
+  const path = require('path');
+
+  const appFolder = path.resolve(process.execPath, '..');
+  const rootAtomFolder = path.resolve(appFolder, '..');
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+  const exeName = path.basename(process.execPath);
+
+  const spawn = function(command, args) {
+      let spawnedProcess, error;
+
+      try {
+          spawnedProcess = ChildProcess.spawn(command, args, {
+              detached: true
+          });
+      } catch (error) {}
+
+      return spawnedProcess;
+  };
+
+  const spawnUpdate = function(args) {
+      return spawn(updateDotExe, args);
+  };
+
+  const squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+      case '--squirrel-install':
+      case '--squirrel-updated':
+          // Optionally do things such as:
+          // - Add your .exe to the PATH
+          // - Write to the registry for things like file associations and
+          //   explorer context menus
+
+          // Install desktop and start menu shortcuts
+          spawnUpdate(['--createShortcut', exeName]);
+
+          setTimeout(application.quit, 1000);
+          return true;
+
+      case '--squirrel-uninstall':
+          // Undo anything you did in the --squirrel-install and
+          // --squirrel-updated handlers
+
+          // Remove desktop and start menu shortcuts
+          spawnUpdate(['--removeShortcut', exeName]);
+
+          setTimeout(application.quit, 1000);
+          return true;
+
+      case '--squirrel-obsolete':
+          // This is called on the outgoing version of your app before
+          // we update to the new version - it's the opposite of
+          // --squirrel-updated
+
+          application.quit();
+          return true;
+  }
+};
+
 
 
   /////////////////
