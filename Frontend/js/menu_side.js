82e1c071784c43c8e09cc37968ff3987f829423e
postni <1524300@stud.hs-mannheim.de>
2017-11-21 14:20:34 +0100

    t
    t

--- Frontend/js/menu_side.js
+++ Frontend/js/menu_side.js
@@ -14,3 +14,25 @@ function closeNav_network() {
     document.getElementById("network").style.width = "0";
 }
 
+
+// Slider1
+var prob = document.getElementById("probability");
+var output = document.getElementById("demo");
+output.innerHTML = prob.value;
+
+prob.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+// Slider2
+var damage = document.getElementById("damage");
+var outputd = document.getElementById("demod");
+outputd.innerHTML = damage.value;
+
+damage.oninput = function() {
+  
+  if(this.value===1){
+    outputd.innerHTML = 'schlecht';
+  }
+    outputd.innerHTML = this.value;
+}
